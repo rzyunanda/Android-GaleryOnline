b2e317cbe321e639b42cc05a02bd7aeb7e1b06ef
rzyunanda <rzyunanda@gmail.com>
2018-12-15 23:30:10 +0700

    api

--- app/src/main/java/pemrogramanmobile/galeryonline/Api/ApiGalery.java
+++ app/src/main/java/pemrogramanmobile/galeryonline/Api/ApiGalery.java
@@ -1,5 +1,14 @@
 package pemrogramanmobile.galeryonline.Api;
 
+import pemrogramanmobile.galeryonline.GaleryData;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiGalery {
+    @GET("/api/galeries")
+    Call<GaleryData> getGaleryData();
+
+
 
 }
